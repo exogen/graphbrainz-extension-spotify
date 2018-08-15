f7c2ec59fb5871edc9539ae6ab6394a0a6caa1d8
Brian Beck <exogen@gmail.com>
2018-08-15 01:55:07 -0700

    Add a test for the recording extension

---
+++ test/_helpers.js
@@ -0,0 +1,32 @@
+import { graphql } from 'graphql'
+import MusicBrainz from 'graphbrainz/lib/api'
+import baseSchema, { createSchema } from 'graphbrainz/lib/schema'
+import { createContext } from 'graphbrainz/lib/context'
+import extension from '../src'
+
+const NOCK_MODE = process.env.NOCK_MODE || 'play'
+const rateLimit = NOCK_MODE === 'play' ? { limit: Infinity, period: 0 } : {}
+const client = new MusicBrainz({ ...rateLimit })
+export const options = {
+  client,
+  spotify: { ...rateLimit },
+  extensions: [extension]
+}
+export const schema = createSchema(baseSchema, options)
+export const context = createContext(options)
+
+export function runQuery(query, variables) {
+  return graphql(schema, query, null, context, variables)
+}
+
+export function testQuerySnapshot(t, query, variables) {
+  return runQuery(query, variables).then(
+    result => {
+      if (result.errors) {
+        result.errors.forEach(error => t.log(error))
+      }
+      t.snapshot(result)
+    },
+    err => t.snapshot(err)
+  )
+}
---
+++ test/schema.test.js
@@ -0,0 +1,43 @@
+import test from 'ava'
+import { setupTests } from 'ava-nock'
+import { testQuerySnapshot } from './_helpers'
+
+setupTests()
+
+test(
+  'MusicBrainz recordings have a spotify track field',
+  testQuerySnapshot,
+  `
+    {
+      lookup {
+        recording(mbid: "6c128cd9-94da-44fe-b74f-b68079fb1606") {
+          spotify {
+            title
+            audioFeatures {
+              acousticness
+              danceability
+              duration
+              energy
+              instrumentalness
+              keyName
+              liveness
+              loudness
+              mode
+              speechiness
+              tempo
+              timeSignature
+              valence
+            }
+            artists {
+              name
+              relatedArtists {
+                name
+                href
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+)
